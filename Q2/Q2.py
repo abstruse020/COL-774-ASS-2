86d591395c351935c3b31be2f0728126e73caf4d
HarsH <bokharsh@gmail.com>
2021-10-05 21:47:30 +0530

    report + minor + 2 b left

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import cvxopt
 from cvxopt import matrix, solvers
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +7,8 @@ import pandas as pd
 
 #%%
 ### Variables
-test_path = 'Q2/train.csv'
+train_path = '~/IITD/COL774-ML/Assignment2/Q2/train.csv'
+test_path = '~/IITD/COL774-ML/Assignment2/Q2/test.csv'
 d = 5
 
 def read_it(path):
@@ -21,20 +21,22 @@ def read_it(path):
 
     return train_data
 
-train_data = read_it(test_path)
+def preprocessing(data):
+    X_data = data.iloc[:,0:-1]
+    X_data = X_data / 255
+    X_data = X_data.to_numpy()
+    Y_data = data.iloc[:,-1]
+    Y_data = (Y_data - 5.5)*2
+    Y_data = Y_data.tolist()
+    return X_data, Y_data
 
+train_data = read_it(train_path)
 
-print('Filtered data shape',train_data.shape, '\nLast col after:', train_data.columns[-1])
-print('uniq labels:', set(train_data.labels))
 #%%
-X_data = train_data.iloc[:10,0:-1]
-X_data = X_data / 255
-Y_data = train_data.iloc[:10,-1]
-Y_data = (Y_data - 5.5)*2
-Y_data = Y_data.tolist()
+X_data , Y_data = preprocessing(train_data)
 
-print(X_data.head()) 
-print(Y_data.head())
+print(X_data[:5]) 
+print(Y_data[:5])
 
 #%%
 
@@ -44,18 +46,29 @@ class SVM_Linear:
         self.Y = Y
         self.c = c
         self.m, self.n = X.shape
-        self.aplhas = [0]*len(Y)
+        self.alphas = [0]*len(Y)
         
-    def make_parameters(self):
+    def gaussian_kernel(self, x, z, sigma_sq = 10):
+        x1 = np.linalg.norm(x - z)
+        val = np.exp(-1* x1/(2* sigma_sq))
+        return val
+        
+    def make_parameters(self, kernel = 'Linear'):
         print("Making parameeters")
         P = np.zeros((self.m, self.m))
         for i in range(self.m):
             for j in range(self.m):
-                temp = self.X[i].T @ self.X[j]
+                temp = 1
+                if kernel == 'Linear':
+                    temp = self.X[i].T @ self.X[j]
+                elif kernel == 'Gaussian':
+                    temp = self.gaussian_kernel(self.X[i], self.X[j])
                 P[i,j] = self.Y[i] * self.Y[j] * temp
         q = np.array([1]*self.m)
-        G = np.array([ [1]*self.m, [-1]*self.m ])
-        h = np.array([self.c, 0.0])
+        G1 = np.diag([1]*self.m)
+        G2 = np.diag([-1]*self.m)
+        G = np.append(G1, G2, axis = 0)
+        h = np.append([self.c]*self.m, [0.0]*self.m, axis =0)
         A = np.array([self.Y])
         b = np.array([0.0])
         
@@ -67,19 +80,22 @@ class SVM_Linear:
         b = matrix(b, tc='d')
         return P,q,G,h,A,b
     
-    def learn(self):
-        P, q, G, h, A, b = self.make_parameters()
+    def learn(self, kernel = 'Linear'):
+        t1 = datetime.datetime.now()
+        P, q, G, h, A, b = self.make_parameters(kernel)
+        t2 = datetime.datetime.now()
+        print('Time to make params:', t2 -t1)
+        print('Solving Started->')
         sol = solvers.qp(P, q, G, h, A, b)
-        print("Solving Complete")
-        self.aplhas = sol['x']
+        self.alphas = sol['x']
         print('Primal objective:', sol['primal objective'])
-        print('Alphas:', self.aplhas)
-        return self.aplhas
+        print('Alphas:\n', self.alphas[:10])
+        return self.alphas
     
     def calc_parameters(self):
         W = np.zeros(self.n)
         for i in range(self.m):
-            W += self.aplhas[i] * self.Y[i] * self.X[i]
+            W += self.alphas[i] * self.Y[i] * self.X[i]
         b1_min = 10000000
         b2_max = 0
         for i in range(self.m):
@@ -93,7 +109,7 @@ class SVM_Linear:
         b = -1.0 * (b1_min + b2_max)/2
         return (W, b)
     
-    def accuracy(Y_prediction, Y_label):
+    def accuracy(self, Y_prediction, Y_label):
         s =0
         for y_hat, y in zip(Y_prediction, Y_label):
             if y_hat == y:
@@ -103,24 +119,121 @@ class SVM_Linear:
     def predict(self, X_test, Y_test = []):
         W, b = self.calc_parameters()
         Y_prediction = X_test @ W + b
-        print(Y_prediction)
+        Y_prediction = [ 1 if y_hat>0 else -1 for y_hat in Y_prediction]
+        print('Accuracy:',self.accuracy(Y_prediction, Y_test))
         return Y_prediction
-
+    
+    def predict_gaussian(self, X_test, Y_test = []):
+        Y_prediction = np.zeros(X_test.shape[0])
+        b1_min = 1000000
+        b2_max = 0
+        for j in range(self.m):
+            comp = 0
+            for i in range(self.m):    
+                comp += self.alphas[i] *self.Y[i] * self.gaussian_kernel(self.X[i], self.X[j])
+            if self.Y[j] == 1:
+                if b1_min > comp:
+                    b1_min = comp
+            else:
+                if b2_max < comp:
+                    b2_max = comp
+        b = -1* (b1_min + b2_max)/2
+        
+        for i in range(X_test.shape[0]):
+            for j in range(self.m):
+                Y_prediction[i] += self.alphas[j] * self.Y[j] * self.gaussian_kernel(self.X[j], X_test[i]) + b
+        Y_prediction = [ 1 if y_hat>0 else -1 for y_hat in Y_prediction]
+        if len(Y_test) == len(X_test):
+            print('Accuracy:', self.accuracy(Y_prediction, Y_test))
+        return Y_prediction
+        
+                    
 
 #%%
 
 svm = SVM_Linear(X_data, Y_data, 1.0)
 
-svm.learn()
+alphas = svm.learn()
+
+#%%
+
+#print(max(alphas))
+svm.predict(X_data[:], Y_data[:])
+
+#%%
+
+test_data = read_it(test_path)
+
+X_test_data, Y_test_data = preprocessing(test_data)
+
+#%%
+test_predictions = svm.predict(X_test_data, Y_test_data)
+
+#%%
+####
+# Things left -->
+# 1. Calculate the support vectors 
+# 2. Check if Epsilon_i is required or not ( as only c came and not epsilon)
+
+svm_gaussian = SVM_Linear(X_data, Y_data, 1.0)
+
+gau_alpha = list(svm_gaussian.learn('Gaussian'))
+
+#%%
+        
+predict_gau = svm_gaussian.predict_gaussian(X_data, Y_data)
+
+#%%
+predict_test_gau = svm_gaussian.predict_gaussian(X_test_data, Y_test_data)
+#%%
+
+zero_count =0
+for al in alphas:
+    if al == 0:
+        zero_count +=1
+print(min(alphas))
 
 
+#%%
 
+#Part c -- SVM for Linear and Gaussian using LIBSVM
 
+from libsvm.svmutil import svm_problem, svm_parameter, svm_train, svm_predict
 
-
-
-
+def Libsvm_linear(X_train, Y_train, X_test, Y_test):
     
+    prob = svm_problem(Y_train, X_train.tolist())
+    
+    params = svm_parameter("-s 0 -c 1 -t 0")
+    
+    svm_lin = svm_train(prob, params)
+    
+    _, _, prediction = svm_predict(Y_test, X_test, svm_lin)
         
+    return prediction
+
+def Libsvm_gaussian(X_train, Y_train, X_test, Y_test, c, g):
+    
+    prob = svm_problem(Y_train, X_train.tolist())
+    
+    params = svm_parameter("-s 0 -c " + str(c)+" -t 2 -g " + str(g))
+    
+    svm_lin = svm_train(prob, params)
+    
+    _, _, prediction = svm_predict(Y_test, X_test, svm_lin)
         
-        
+    return prediction
+
+#%%
+# On Train data
+
+Libsvm_linear(X_data, Y_data, X_data, Y_data)
+
+_ = Libsvm_gaussian(X_data, Y_data, X_data, Y_data, 10, 0.01)
+
+#%%
+# On Test data
+
+Libsvm_linear(X_data, Y_data, X_test_data, Y_test_data)
+
+_ = Libsvm_gaussian(X_data, Y_data, X_test_data, Y_test_data, 10, 0.01)
